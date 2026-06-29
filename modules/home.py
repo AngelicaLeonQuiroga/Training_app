486ed5fe3bf907da6b95e076f348063cba1d1e7f
Angelica leon <angelicaleonquiroga@gmail.com>
2026-06-29 18:49:57 -0400

    logos de proximamente

--- modules/home.py
+++ modules/home.py
@@ -11,6 +11,7 @@ def home_screen():
     
     img_fire = get_image_base64("assets/fire.png")
     img_bio = get_image_base64("assets/biosecurity.png")
+    img_chemical = get_image_base64("assets/chemical.png")
     learning = get_image_base64("assets/learning.png")
     farmer = get_image_base64("assets/farmer.png")
     check = get_image_base64("assets/check.png")
@@ -42,12 +43,10 @@ def home_screen():
     
     # -------- CARD 1 --------
     with col1:
-
         st.markdown(f"""
         <div class="card clickable">
             <h3>Seguridad contra incendios</h3>
             <img src="data:image/png;base64,{img_fire}" />
-            <p>Aprende a prevenir incendios y usar extintores.</p>
         </div>
         """, unsafe_allow_html=True)
         st.markdown("---")
@@ -62,17 +61,24 @@ def home_screen():
 
     # -------- CARD 2 --------
     with col2:
+        st.markdown("""**PROXIMAMENTE**""")
+        col1, col2 = st.columns([1,1])
+        with col1:
+             
+             st.markdown(f"""
+            <div>
+                <img src="data:image/png;base64,{img_bio}" />
+            </div>
+            """, unsafe_allow_html=True)
+             st.markdown("")
+        with col2:
+             st.markdown(f"""
+            <div>
+                <img src="data:image/png;base64,{img_chemical}" />
+            </div>
+            """, unsafe_allow_html=True)
 
- #       st.markdown(f"""
-  #      <div class="card clickable">
-   #         <h3>Biosecurity</h3>
-    #        <img src="data:image/png;base64,{img_bio}" />
-      #      <p>Learn how to biosecurity.</p>
-     #   </div>
-       # """, unsafe_allow_html=True)
-        st.markdown("---")
-
- #       if st.button("Start", key="bio", use_container_width=True):
+ #       if st.button("Start", key="bio", width="stretch"):
   #          st.session_state["selected_training"] = "bio"
    #         st.session_state["course_name"] = "Biosecurity"
         # GUARDAR HORA DE INICIO
